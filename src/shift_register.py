3988c0b7e5d43eefdf06c201860b11341cec93ac
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2022-04-03 18:38:28 +0200

    Shift register v1.0

--- src/shift_register.py
+++ src/shift_register.py
@@ -1,42 +1,89 @@
+#!/usr/bin/env python
+
+"""This module controls the physical object 'shift register'"""
+
 import RPi.GPIO as GPIO
 
-
-def shift_register(byte: bin):
-    for x in range(30):
-        GPIO.output(data, (byte >> x) & 1)
-        GPIO.output(clock, 1)
-        GPIO.output(clock, 0)
-    GPIO.output(latch, 1)
-    GPIO.output(latch, 0)
+from file_reader import FileReader
 
 
 class ShiftRegister:
     """
-    123
+    This class is used to handle shift registers.
     """
 
     __data: int = 0
-    """Number of data pin"""
+    """Data pin number"""
 
     __clock: int = 0
-    """Number of clock pin"""
+    """Clock pin number"""
 
     __latch: int = 0
-    """Number of latch pin"""
+    """Latch pin number"""
 
-    def __init__(self) -> None:
+    __modules: int = 0
+    """Number of shift registers"""
+
+    def __init__(self, modules: int = 1) -> None:
+        """
+        This constructor prepares the raspberry to run and writes the data needed to use the shift register.
+
+        :param modules: int | Number of shift registers to control
+        :return: None
+        """
+        if modules <= 0:
+            FileReader.append_error('Number of modules in ShiftRegister class must be greater than 0.')
+            raise ValueError('Number of modules must be greater than 0.')
+
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
 
-        data: int = 19
-        clock: int = 13
-        latch: int = 6
+        file = FileReader()
+        file_dict = file.read_pins()
 
-        GPIO.setup(data, GPIO.OUT)
-        GPIO.setup(clock, GPIO.OUT)
-        GPIO.setup(latch, GPIO.OUT)
+        if 'data' and 'clock' and 'latch' not in file_dict:
+            FileReader.append_error('Not enough data in the pins.json file.')
+            raise Exception('Not enough data in the pins file.')
 
-    def __del__(self):
+        self.__data = file_dict["data"]
+        self.__clock = file_dict["clock"]
+        self.__latch = file_dict["latch"]
+        self.__modules = modules
+
+        GPIO.setup(self.__data, GPIO.OUT)
+        GPIO.setup(self.__clock, GPIO.OUT)
+        GPIO.setup(self.__latch, GPIO.OUT)
+
+    def clear(self) -> None:
+        """
+        This function clear all shift registers.
+
+        :return: None
+        """
+        for x in range(self.__modules):
+            self.run(0x00000000)
+
+    def run(self, byte: bin) -> None:
+        """
+        Runs one complete cycle of data movements in a shift register.
+
+        :param byte: bin | Binary data to display
+        :return: None
+        """
+        for x in range(8 * self.__modules):
+            GPIO.output(self.__data, (byte >> x) & 1)
+            GPIO.output(self.__clock, 1)
+            GPIO.output(self.__clock, 0)
+        GPIO.output(self.__latch, 1)
+        GPIO.output(self.__latch, 0)
+
+    def __del__(self) -> None:
+        """
+        This destructor clears the shift registers and turns off the control pins.
+
+        :return: None
+        """
+        self.clear()
         GPIO.output(self.__data, 0)
         GPIO.output(self.__clock, 0)
         GPIO.output(self.__latch, 0)
---
+++ tests/test_shift_register.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
+
+import import_from_root
+from src.shift_register import ShiftRegister
+
+
+class TestShiftRegister(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
